5bd8d06429e5a92168055c0a91c2a93068770804
Alexandr <alex903_90@mail.ru>
2021-02-11 23:05:45 +0700

    4.3 шаг 8

--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,5 @@
 from selenium.webdriver.common.by import By
 
-
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
-
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
@@ -15,3 +10,8 @@ class ProductPageLocators():
     MESSAGE_SUCCESS = (By.CSS_SELECTOR, ".alertinner > strong")
     NAME_PRODUCT = (By.CSS_SELECTOR, ".product_main > h1")
     PRICE_PRODUCT = (By.CSS_SELECTOR, ".product_main >.price_color")
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    # LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
